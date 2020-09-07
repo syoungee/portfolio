07af809719693ebe39c9eaa166844a5c735f087b
syoungee <syoung_ee@naver.com>
2020-09-07 17:34:47 +0900

    JavaScript: Navbar

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// Make navbar transparent when it is on the top
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
+});
