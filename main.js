8545d90af021823c8d92eed3b50d9ae9d251aa18
syoungee <syoung_ee@naver.com>
2020-09-09 16:04:48 +0900

    JS: Transparent

--- main.js
+++ main.js
@@ -33,3 +33,11 @@ function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
 }
+
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
